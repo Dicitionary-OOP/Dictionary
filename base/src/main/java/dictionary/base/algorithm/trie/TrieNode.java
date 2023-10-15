2fbe60129362467ec0956082144b86c58219f975
hoangvanphi2004 <hoangvanphi2004@gmail.com>
2023-10-15 09:53:45 +0700

    TrieChange

--- base/src/main/java/dictionary/base/algorithm/trie/TrieNode.java
+++ base/src/main/java/dictionary/base/algorithm/trie/TrieNode.java
@@ -1,16 +1,19 @@
 package dictionary.base.algorithm.trie;
 
+
 /**
  * Represents a node in a Trie data structure.
- *
- * @param <V> The type of data associated with the TrieNode.
  */
-public class TrieNode<V>
+public class TrieNode
 {
-    private final int ALPHABET_SIZE = 26;
-    private TrieNode<V> parent;
-    private V data;
-    private final TrieNode<V>[] childs;
+    private final int ASCIISIZE = 128;
+    private char characterTable[] = {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', ' ', '-', '\''};
+    private Integer mapToCharacterPosition[] = new Integer[ASCIISIZE];
+    private TrieNode parent;
+    private final TrieNode[] childs;
     private boolean isEnd;
 
     /**
@@ -19,9 +22,9 @@ public class TrieNode<V>
     public TrieNode()
     {
         parent = null;
-        data = null;
-        childs = new TrieNode[ALPHABET_SIZE];
-        for (int i = 0; i < ALPHABET_SIZE; i++) {
+        childs = new TrieNode[characterTable.length];
+        for (int i = 0; i < characterTable.length; i++) {
+            mapToCharacterPosition[(int)characterTable[i]] = i;
             childs[i] = null;
         }
         isEnd = false;
@@ -33,12 +36,12 @@ public class TrieNode<V>
      * @param nextChar The character to find or create the child node.
      * @return The next child TrieNode associated with the character.
      */
-    public TrieNode<V> next(final char nextChar)
+    public TrieNode next(final char nextChar)
     {
-        if (childs[(int)nextChar - (int)'a'] == null) {
-            childs[(int)nextChar - (int)'a'] = new TrieNode<>();
+        if (childs[mapToCharacterPosition[nextChar]] == null) {
+            childs[mapToCharacterPosition[nextChar]] = new TrieNode();
         }
-        return childs[(int)nextChar - (int)'a'];
+        return childs[mapToCharacterPosition[nextChar]];
     }
 
     /**
@@ -46,7 +49,7 @@ public class TrieNode<V>
      *
      * @return The array of child TrieNodes.
      */
-    public TrieNode<V>[] getChilds() { return childs; }
+    public TrieNode[] getChilds() { return childs; }
 
     /**
      * Gets the child node at a specific index.
@@ -54,7 +57,7 @@ public class TrieNode<V>
      * @param i The index of the child node to retrieve.
      * @return The child TrieNode at the specified index.
      */
-    public TrieNode<V> getChild(final int i) { return childs[i]; }
+    public TrieNode getChild(final int i) { return childs[i]; }
 
     /**
      * Sets the child node associated with a character to null.
@@ -63,22 +66,8 @@ public class TrieNode<V>
      */
     public void setChildToNull(final char currentChar)
     {
-        this.childs[(int)currentChar - (int)'a'] = null;
+        this.childs[mapToCharacterPosition[currentChar]] = null;
     }
-
-    /**
-     * Gets the data associated with the TrieNode.
-     *
-     * @return The data associated with the TrieNode.
-     */
-    public V getData() { return data; }
-
-    /**
-     * Sets the data associated with the TrieNode.
-     *
-     * @param data The data to set.
-     */
-    public void setData(final V data) { this.data = data; }
 
     /**
      * Sets the flag to indicate whether the TrieNode represents the end of a word.
@@ -99,12 +88,12 @@ public class TrieNode<V>
      *
      * @return The parent TrieNode of the current TrieNode.
      */
-    public TrieNode<V> getParent() { return parent; }
+    public TrieNode getParent() { return parent; }
 
     /**
      * Sets the parent TrieNode of the current TrieNode.
      *
      * @param parentNode The parent TrieNode to set.
      */
-    public void setParent(final TrieNode<V> parentNode) { this.parent = parentNode; }
+    public void setParent(final TrieNode parentNode) { this.parent = parentNode; }
 }
