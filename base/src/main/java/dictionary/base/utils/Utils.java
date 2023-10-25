faf5c6a1167e7347c18f300fb619af0c9215ceb7
png261 <nhphuong.code@gmail.com>
2023-10-25 19:24:16 +0700

    feat(Utils): isNetworkConnected

--- base/src/main/java/dictionary/base/utils/Utils.java
+++ base/src/main/java/dictionary/base/utils/Utils.java
@@ -1,7 +1,20 @@
 package dictionary.base.utils;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 public class Utils {
     public static String getResource(String path) {
         return Utils.class.getResource(path).getPath();
+    }
+
+    public static boolean isNetworkConnected() {
+        try {
+            InetAddress address = InetAddress.getByName("www.google.com");
+
+            return !address.equals("");
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
