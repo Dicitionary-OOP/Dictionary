4e7ba097437a00e8ded6946cfe7b18cb73f9c1a4
Phuong Nguyen <nhphuong.code@gmail.com>
2023-10-25 08:30:03 +0700

    fix: remove Exception

--- base/src/main/java/dictionary/base/exceptions/AddExampleException.java
+++
@@ -1,7 +0,0 @@
-package dictionary.base.exceptions;
-
-public class AddExampleException extends java.lang.Exception {
-    public AddExampleException(String message) {
-        super(message);
-    }
-}
