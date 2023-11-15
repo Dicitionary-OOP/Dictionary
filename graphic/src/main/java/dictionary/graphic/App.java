9dd769b5cc384062ec2f6b151b50e9e32e0cac69
tsun165 <sondo16052004@gmail.com>
2023-11-15 17:28:03 +0700

    fix: update logo set

--- graphic/src/main/java/dictionary/graphic/App.java
+++ graphic/src/main/java/dictionary/graphic/App.java
@@ -1,15 +1,21 @@
 package dictionary.graphic;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
+import dictionary.base.utils.Utils;
 import dictionary.graphic.controllers.SceneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(final Stage stage) throws IOException {
+        stage.setTitle("Dictionary");
+        Image logoApp = new Image(new FileInputStream((Utils.getResource("/dictlogo.png"))));
+        stage.getIcons().add(logoApp);
         SceneController.getInstance().init(stage);
         SceneController.getInstance().switchScene(View.Main.fxml());
     }
