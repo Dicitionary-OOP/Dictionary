60ebc2b40688f6360a69121ce838fb1d3d91cecf
png261 <nhphuong.code@gmail.com>
2023-11-17 23:17:15 +0700

    chore: add logo

--- graphic/src/main/java/dictionary/graphic/App.java
+++ graphic/src/main/java/dictionary/graphic/App.java
@@ -1,26 +1,24 @@
 package dictionary.graphic;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-
 import dictionary.base.utils.Utils;
 import dictionary.graphic.controllers.SceneController;
+import java.io.FileInputStream;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class App extends Application {
-    @Override
-    public void start(final Stage stage) throws IOException {
+public class App extends Application
+{
+    @Override public void start(final Stage stage) throws IOException
+    {
         stage.setTitle("Dictionary");
-        Image logoApp = new Image(new FileInputStream((Utils.getResource("/dictlogo.png"))));
-        stage.getIcons().add(logoApp);
+        stage.getIcons().add(new Image(new FileInputStream((Utils.getResource("/icon/logo.png")))));
+
         SceneController.getInstance().init(stage);
         SceneController.getInstance().switchScene(View.Main.fxml());
     }
 
-    public static void main(final String[] args) {
-        launch(args);
-    }
+    public static void main(final String[] args) { launch(args); }
 }
