560f2ecd7fcf54713be4cec2989326328b8fdd2c
Vu Tung Lam <vutunglaminfo@gmail.com>
2023-11-28 11:28:21 +0700

    feat: completed search functionality in English-Vietnamese view

--- graphic/src/main/java/dictionary/graphic/controllers/EnglishVietnameseController.java
+++ graphic/src/main/java/dictionary/graphic/controllers/EnglishVietnameseController.java
@@ -1,28 +1,25 @@
 package dictionary.graphic.controllers;
 
 import dictionary.base.api.TextToSpeechOfflineAPI;
-import dictionary.base.database.Database;
 import dictionary.base.database.DictionaryDatabase;
 import dictionary.base.Example;
 import dictionary.base.Explain;
 import dictionary.base.Word;
-import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 
 import dictionary.base.Dictionary;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
 import javafx.scene.layout.VBox;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EnglishVietnameseController {
-    private Dictionary dictionary;
     @FXML
     private Button speech;
     @FXML
@@ -33,54 +30,182 @@ public class EnglishVietnameseController {
     private VBox explainField;
 
     @FXML
+    private TextField searchBar;
+
+    @FXML
+    private ListView<String> suggestedWords;
+
+    @FXML
     private void initialize() {
-        try {
-            showDetail(Dictionary.getInstance().getAllWords().get(2).get(1));
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void showDetail(String wordID) {
-        Thread thread = new Thread(() -> {
-            try {
-                Word word = Dictionary.getInstance().getDatabase().getWordByWordID(wordID);
-                DictionaryDatabase database = Dictionary.getInstance().getDatabase();
-
-                wordField.setText(word.getWord());
-                pronounceField.setText('[' + word.getPronunce() + ']');
-                ArrayList<Explain> explains = database.getExplainsByWordID(word.getWordID());
-                for (Explain explain : explains) {
-                    Label type = new Label(explain.getType() + "\n");
-                    Label meaning = new Label("\t" + explain.getMeaning() + "\n");
-                    type.getStyleClass().add("level1");
-                    type.setWrapText(true);
-                    explainField.getChildren().add(type);
-                    meaning.getStyleClass().add("level2");
-                    meaning.setWrapText(true);
-                    explainField.getChildren().add(meaning);
-                    ArrayList<Example> examples = database.getExamples(explain.getExplainID());
-                    for (Example example : examples) {
-                        Label exampleText = new Label("\t\t" + example.getExample() + "\n");
-                        exampleText.getStyleClass().add("level3");
-                        exampleText.setWrapText(true);
-                        explainField.getChildren().add(exampleText);
-                    }
+        suggestedWords.setCellFactory((ListView<String> list) -> {
+            ListCell<String> cell = new ListCell<String>() {
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setText(item);
                 }
-            } catch (SQLException e) {
-                wordField.setText(null);
-                explainField.getChildren().clear();
-                Label error = new Label("Đã có lỗi xảy ra");
-                explainField.getChildren().add(error);
+            };
+
+            cell.setOnMouseClicked((MouseEvent event) -> {
+                pickCurrentSuggestion();
+            });
+
+            cell.setOnTouchReleased((TouchEvent event) -> {
+                pickCurrentSuggestion();
+            });
+
+            return cell;
+        });
+
+        suggestedWords.setOnKeyPressed((KeyEvent event) -> {
+            switch (event.getCode()) {
+                case ENTER:
+                    pickCurrentSuggestion();
+                    break;
+
+                case UP:
+                case KP_UP:
+                    if (suggestedWords.getSelectionModel().getSelectedIndex() == 0) {
+                        focusOnSearchBar();
+                        suggestedWords.getSelectionModel().clearSelection();
+                    }
+                    break;
             }
         });
-        thread.setDaemon(true);
-        thread.start();
+    }
+
+    private void showDetail(Word word) {
+        try {
+            DictionaryDatabase database = Dictionary.getInstance().getDatabase();
+
+            wordField.setText(word.getWord());
+            pronounceField.setText('[' + word.getPronunce() + ']');
+            ArrayList<Explain> explains = database.getExplainsByWordID(word.getWordID());
+            for (Explain explain : explains) {
+                Label type = new Label(explain.getType() + "\n");
+                Label meaning = new Label("\t" + explain.getMeaning() + "\n");
+                type.getStyleClass().add("level1");
+                type.setWrapText(true);
+                explainField.getChildren().add(type);
+                meaning.getStyleClass().add("level2");
+                meaning.setWrapText(true);
+                explainField.getChildren().add(meaning);
+                ArrayList<Example> examples = database.getExamples(explain.getExplainID());
+                for (Example example : examples) {
+                    Label exampleText = new Label("\t\t" + example.getExample() + "\n");
+                    exampleText.getStyleClass().add("level3");
+                    exampleText.setWrapText(true);
+                    explainField.getChildren().add(exampleText);
+                }
+            }
+        } catch (SQLException e) {
+            wordField.setText(null);
+            explainField.getChildren().clear();
+            Label error = new Label("Đã có lỗi xảy ra");
+            explainField.getChildren().add(error);
+        }
     }
     @FXML
     private void onPlayAudioButton(){
         Thread thread = new Thread(() -> TextToSpeechOfflineAPI.getTextToSpeech(wordField.getText()));
         thread.setDaemon(true);
         thread.start();
+    }
+
+    @FXML
+    private void onSearchBarInputTextChanged() {
+        updateSuggestions();
+    }
+
+    @FXML
+    private void onSearchBarKeyPressed(KeyEvent event) {
+        switch (event.getCode()) {
+            case ENTER:
+                pickBestMatch();
+                break;
+
+            case DOWN:
+            case KP_DOWN:
+                useSuggestions();
+                break;
+        }
+    }
+
+    /**
+     * Updates word suggestions.
+     */
+    private void updateSuggestions() {
+        try {
+            DictionaryDatabase database = Dictionary.getInstance().getDatabase();
+            List<String> words = database.getWordsStartWith(searchBar.getText());
+            suggestedWords.setItems(
+                FXCollections.observableList(words)
+            );
+        } catch (SQLException e) {
+            handleSQLException(e);
+        }
+    }
+
+    /**
+     * This function gets called when the user no longer
+     * types in the word manually, and instead wants to
+     * select one of the suggestions.
+     */
+    private void useSuggestions() {
+        if (suggestedWords.getItems().isEmpty()) {
+            return;
+        }
+        suggestedWords.requestFocus();
+        suggestedWords.getSelectionModel().select(0);
+    }
+
+    /**
+     * Loads the currently-selected suggestion into the
+     * search bar, and display its word's details.
+     */
+    private void pickCurrentSuggestion() {
+        String wordString = suggestedWords.getSelectionModel().getSelectedItem();
+
+        try {
+            DictionaryDatabase database = Dictionary.getInstance().getDatabase();
+            Word word = database.getWordObjectByWord(wordString);
+            showDetail(word);
+        } catch (SQLException e) {
+            handleSQLException(e);
+        }
+
+        searchBar.setText(wordString);
+        focusOnSearchBar();
+    }
+
+    /**
+     * Shows details of the best-match word, i.e. the word that is on top
+     * of search results (suggestions).
+     */
+    private void pickBestMatch() {
+        if (suggestedWords.getItems().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Lỗi");
+            alert.setHeaderText(String.format("Không tìm thấy từ tương tự \'%s\'", searchBar.getText()));
+            alert.setContentText("Vui lòng nhập lại từ đúng hoặc thêm từ mới.");
+            alert.showAndWait();
+            return;
+        }
+        suggestedWords.getSelectionModel().select(0);
+        pickCurrentSuggestion();
+    }
+
+    private void focusOnSearchBar() {
+        searchBar.requestFocus();
+        searchBar.deselect();
+        searchBar.positionCaret(searchBar.getText().length());
+    }
+
+    private static void handleSQLException(SQLException e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Lỗi");
+        alert.setHeaderText("Không thể truy cập dữ liệu");
+        alert.setContentText("Vui lòng thử lại sau");
+        alert.showAndWait();
     }
 }
