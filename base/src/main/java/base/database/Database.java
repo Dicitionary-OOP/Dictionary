4836b21d9a0c331a4441dfcce3740bfba95d7b48
png261 <nhphuong.code@gmail.com>
2023-12-02 23:50:06 +0700

    feat: add update queries

--- base/src/main/java/base/database/Database.java
+++ base/src/main/java/base/database/Database.java
@@ -103,20 +103,6 @@ public class Database {
         return explains;
     }
 
-    public ArrayList<String> getWordsStartWith(final String prefix) throws SQLException {
-        final String sql = "SELECT * FROM words WHERE words.word LIKE ? ORDER BY words.word;";
-        final PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1, prefix + "%");
-
-        final ArrayList<String> words = new ArrayList<>();
-        final ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-            words.add(resultSet.getString("word"));
-        }
-
-        return words;
-    }
-
     public String addWord(final Word word) throws SQLException {
         final StringBuilder query = new StringBuilder();
         query.append("INSERT INTO words (word, pronounce) ");
@@ -132,6 +118,20 @@ public class Database {
 
         final ResultSet resultSet = statement.executeQuery("SELECT MAX(word_id) as word_id FROM words LIMIT 1 ");
         return resultSet.getString("word_id");
+    }
+
+    public void updateWord(final String wordID, final Word word) throws SQLException {
+        final StringBuilder query = new StringBuilder();
+        query.append("UPDATE words");
+        query.append("SET");
+        query.append("word = ?");
+        query.append("pronounce = ?");
+        query.append("WHERE word_id = ?");
+        final PreparedStatement preparedStatement = connection.prepareStatement(query.toString());
+        preparedStatement.setString(1, word.getWord());
+        preparedStatement.setString(2, word.getPronunce());
+        preparedStatement.setString(3, wordID);
+        preparedStatement.executeUpdate();
     }
 
     public void removeWord(final String wordID) throws SQLException {
@@ -170,6 +170,22 @@ public class Database {
         preparedStatement.executeUpdate();
     }
 
+    public void updateExplain(final String explainID, final Explain explain) throws SQLException {
+        final StringBuilder query = new StringBuilder();
+        query.append("UPDATE explains");
+        query.append("SET");
+        query.append("word_id = ? ,");
+        query.append("type = ? ,");
+        query.append("meaning = ?");
+        query.append("WHERE explain_id = ?");
+        final PreparedStatement preparedStatement = connection.prepareStatement(query.toString());
+        preparedStatement.setString(1, explain.getWordID());
+        preparedStatement.setString(2, explain.getType());
+        preparedStatement.setString(3, explain.getMeaning());
+        preparedStatement.setString(4, explainID);
+        preparedStatement.executeUpdate();
+    }
+
     public void addExample(final Example example) throws SQLException {
         final PreparedStatement preparedStatement = connection.prepareStatement(
                 "INSERT INTO examples (explain_id, example, translate) VALUES (?, ?, ?)");
@@ -183,6 +199,22 @@ public class Database {
         final PreparedStatement preparedStatement = connection
                 .prepareStatement("DELETE FROM examples WHERE example_id = ?");
         preparedStatement.setString(1, exampleID);
+        preparedStatement.executeUpdate();
+    }
+
+    public void updateExample(final String exampleID, final Example example) throws SQLException {
+        final StringBuilder query = new StringBuilder();
+        query.append("UPDATE examples");
+        query.append("SET");
+        query.append("explain_id = ? ,");
+        query.append("example = ? ,");
+        query.append("translate = ?");
+        query.append("WHERE example_id = ?");
+        final PreparedStatement preparedStatement = connection.prepareStatement(query.toString());
+        preparedStatement.setString(1, example.getExampleID());
+        preparedStatement.setString(2, example.getExample());
+        preparedStatement.setString(3, example.getTranslate());
+        preparedStatement.setString(4, exampleID);
         preparedStatement.executeUpdate();
     }
 
